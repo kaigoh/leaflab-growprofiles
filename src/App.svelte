1ab2d70bfbebc011926cb56b40e5f7a3b3718c49
Kai Gohegan <kai@gohegan.uk>
2023-08-11 13:51:14 +0100

    Logo

--- src/App.svelte
+++ src/App.svelte
@@ -164,7 +164,7 @@
 <div class="pt-3">
   <div class="row text-center mb-3">
     <div class="col-sm-8">
-      <img src="img/logo.svg" class="img-fluid" />
+      <img src="img/logo.svg" class="img-fluid mx-auto d-block" />
     </div>
   </div>
   <div class="row text-center">
